e071104def88afd24f88260d1c3b606d04970c7a
Alfred5779 <wfl88377919@qq.com>
2025-05-13 19:22:39 +0800

    5.13

---
+++ 1.py
@@ -0,0 +1,14 @@
+  
+for i in range(2000, 3201):
+        if i % 7 == 0 and i % 5 != 0:
+            print(i, end=',')
+                
+
+
+
+def timu4():
+    a=input()
+    b=tuple(a.split (','))
+    c=a.split(',')
+    print(b,c)
+timu4()
---
+++ 2.py
@@ -0,0 +1,5 @@
+num=1
+n=int(input("请输入一个数字："))
+for i in range(1,n+1):
+    num=num*i
+print(num)
