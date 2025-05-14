83b89a790940ca00ebfe658e4260ea36aa68b65b
Alfred5779 <wfl88377919@qq.com>
2025-05-14 20:36:46 +0800

    10-20更新完毕

--- 11-20/20.py
+++ 11-20/20.py
@@ -2,6 +2,7 @@
 Define a class with a generator which can iterate the numbers, which are divisible by 7, between a given range 0 and n.
 使用生成器定义一个类,该生成器可以迭代给定范围0和n之间的可被7整除的数字。
 """
+
 #本人答案
 class task():
     def find(self,n):
